16f3d7ef7c48ef292be93edd216a8cbffbaec167
KubaZary <kubamusielski@wp.pl>
2023-05-11 21:49:07 +0200

    about me button

--- components/home/TypewriterSection.tsx
+++ components/home/TypewriterSection.tsx
@@ -1,15 +1,19 @@
 "use client"
 import Typewriter from 'typewriter-effect'
 import styles from './TypewriterSection.module.css'
+import Link from 'next/link'
 export default function TypewriterSection() {
   return (<div className={styles.landingSectionFirst}>
-    <div>Hello, Im <span className={styles.textLandingSpecial}>Kuba Musielski</span>,
-    <Typewriter 
-      options={{
-        strings: ['javascript developer', 'react developer', 'nextjs developer'],
-        autoStart: true,
-        loop: true,
-      }}
-    /></div>
+    <div>
+      Hello, Im <span className={styles.textLandingSpecial}>Kuba Musielski</span>,
+      <Typewriter
+        options={{
+          strings: ['javascript developer', 'react developer', 'nextjs developer'],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+      <Link draggable='false' href='/about' className={styles.aboutMeButton}>About me</Link>
+    </div>
   </div>)
 };
