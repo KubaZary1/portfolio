7b83957c485a7ea827d00572fa2b9273f4943a48
KubaZary <kubamusielski@wp.pl>
2023-05-11 21:32:15 +0200

    typewriter effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+"use client"
 import styles from './page.module.css'
+import Typewriter from 'typewriter-effect';
+
 export default function Home() {
   return (
     <>
       <div className={styles.landingSectionFirst}>
-        <div>Hello, Im <span className={styles.textLandingSpecial}>Kuba Musielski</span>, javascript developer.</div>
+        <div>Hello, Im <span className={styles.textLandingSpecial}>Kuba Musielski</span>,
+        <Typewriter 
+          options={{
+            strings: ['javascript developer', 'react developer', 'nextjs developer'],
+            autoStart: true,
+            loop: true,
+          }}
+        /></div>
       </div>
     </>
   )
