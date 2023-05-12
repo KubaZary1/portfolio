99664ef6f606d8e798c528cc9a97cd34e1349be1
KubaZary <kubamusielski@wp.pl>
2023-05-12 18:53:01 +0200

    responsive design for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,27 @@
+"use client";
+
+import {IoEllipsisVertical} from "react-icons/io5"
 import Link from "next/link";
 import styles from "./Navbar.module.css"
+import { useState } from "react";
 
 export default function Navbar() {
+  const [navbarMobileState, setNavbarMobileState] = useState(false)
   const navbarLinks = [
     {path: '/', name: "home"},
     {path: '/about', name: "about"},
   ]
   return (
-    <nav className={styles.navbar}>
+    <>
+    <nav className={navbarMobileState ? `${styles.navbar} ${styles.navbarEnabled}` : styles.navbar}>
       <div><div className={styles.logo}>KUBA MUSIELSKI</div></div>
-      <div>
+      <div className={styles.links}>
         {navbarLinks.map((element, index)=> {
-          return (<Link href={element.path} key={index}>{element.name.toUpperCase()}</Link>)
+          return (<Link onClick={() => setNavbarMobileState(false)} href={element.path} key={index}>{element.name.toUpperCase()}</Link>)
         })}
       </div>
     </nav>
+    <button type="button" className={styles.mobileNavbarButton} onClick={() => setNavbarMobileState(prev => !prev)} title="toggle navbar"><IoEllipsisVertical /></button>
+    </>
   )
 };
