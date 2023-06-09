3ee2fbcf752e5dfe0c4746c597cd38264799a343
KubaZary <kubamusielski@wp.pl>
2023-06-09 21:42:21 +0200

    coverImage in post background color change

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import styles from "./Navbar.module.css"
 import { useState } from "react";
-import buttonMenuIcon from "../public/menu.svg"
+import buttonMenuIcon from "@/public/menu.svg"
 import Image from "next/image";
 
 export default function Navbar() {
@@ -16,7 +16,10 @@ export default function Navbar() {
   return (
     <>
     <nav className={navbarMobileState ? `${styles.navbar} ${styles.navbarEnabled}` : styles.navbar}>
-      <div><div className={styles.logo}>KUBA MUSIELSKI</div></div>
+      <div>
+        {/* TODO: Add <Link /> to logo */}
+        <div className={styles.logo}>KUBA MUSIELSKI</div>
+      </div>
       <div className={styles.links}>
         {navbarLinks.map((element, index)=> {
           return (<Link onClick={() => setNavbarMobileState(false)} href={element.path} key={index}>{element.name.toUpperCase()}</Link>)
